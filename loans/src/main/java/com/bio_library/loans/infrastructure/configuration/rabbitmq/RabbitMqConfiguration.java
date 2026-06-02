8ffc1f8015f817dbfca6f5c9c52fafa5df5b09b9
Sara Bermudez <bermudezalvarezsaraestefania@gmail.com>
2026-06-02 07:49:32 -0500

    Fix jeager tracks

--- loans/src/main/java/com/bio_library/loans/infrastructure/configuration/rabbitmq/RabbitMqConfiguration.java
+++ loans/src/main/java/com/bio_library/loans/infrastructure/configuration/rabbitmq/RabbitMqConfiguration.java
@@ -1,10 +1,9 @@
 package com.bio_library.loans.infrastructure.configuration.rabbitmq;
 
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.amqp.RabbitTemplateConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,9 +16,12 @@ public class RabbitMqConfiguration {
     }
 
     @Bean
-    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    public RabbitTemplate rabbitTemplate(RabbitTemplateConfigurer configurer,
+                                         org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        configurer.configure(rabbitTemplate, connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter());
+        rabbitTemplate.setObservationEnabled(true);
         return rabbitTemplate;
     }
 }
--- notification/src/main/java/com/bio_library/notification/infrastructure/configuration/rabbitmq/RabbitMqConfiguration.java
+++ notification/src/main/java/com/bio_library/notification/infrastructure/configuration/rabbitmq/RabbitMqConfiguration.java
@@ -1,12 +1,9 @@
 package com.bio_library.notification.infrastructure.configuration.rabbitmq;
 
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -43,12 +40,5 @@ public class RabbitMqConfiguration {
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(messageConverter());
-        return rabbitTemplate;
     }
 }
